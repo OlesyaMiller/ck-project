ace658365e74c9a566991c66c45e92badd0e1c1b
Olesya <olesyam.miller@gmail.com>
2025-02-05 15:50:16 -0400

    refactor main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,51 +4,36 @@
     let ratingItem1 = null;
     let ratingItem2 = null;
     let ratingItem3 = null;
+
+    const ratingItems = [ratingItem1, ratingItem2, ratingItem3];
   
   </script>
   
   <div class="space-y-8 p-4">
-    <!-- Item 1 -->
-    <div>
-      <h2 class="text-2xl font-semibold mb-2">Item 1</h2>
-  
-      <Rating bind:rating={ratingItem1} />
-  
-      {#if ratingItem1 !== null}
-        <p class="rating mt-2">{ratingItem1} star{ratingItem1 > 1 ? 's' : ''}</p>
-      {:else}
-        <p class="rating mt-2 text-gray-600">No rating</p>
-      {/if}
-      
-    </div>
-  
-    <!-- Item 2 -->
-    <div>
-      <h2 class="text-2xl font-semibold mb-2">Item 2</h2>
-  
-      <Rating bind:rating={ratingItem2} />
-  
-      {#if ratingItem2 !== null}
-        <p class="rating mt-2">{ratingItem2} star{ratingItem2 > 1 ? 's' : ''}</p>
-      {:else}
-        <p class="rating mt-2 text-gray-600">No rating</p>
-      {/if}
-  
-    </div>
-  
-    <!-- Item 3 -->
-    <div>
-      <h2 class="text-2xl font-semibold mb-2">Item 3</h2>
-  
-      <Rating bind:rating={ratingItem3} />
-  
-      {#if ratingItem3 !== null}
-        <p class="rating mt-2">{ratingItem3} star{ratingItem3 > 1 ? 's' : ''}</p>
-      {:else}
-        <p class="rating mt-2 text-gray-600">No rating</p>
-      {/if}
-  
-    </div>
+    
+    
+    {#each [1, 2, 3] as n}
+
+        <div class="flex">
+
+            <h2 class="text-2xl font-semibold mb-2">Item {n}</h2>
+    
+            <Rating bind:rating={ratingItems[n-1]}/>
+        
+            {#if ratingItems[n-1] !== null}
+
+                <p class="rating mt-2">{ratingItems[n-1]} star{ratingItems[n-1] > 1 ? 's' : ''}</p>
+
+            {:else}
+
+                <p class="rating mt-2 text-gray-600">No rating</p>
+
+            {/if}
+
+        </div>
+
+    {/each}
+ 
   </div>
   
   
